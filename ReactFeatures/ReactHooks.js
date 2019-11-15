578f98c61d930ba7c733544e083c689157b1500c
infant_sabin <a-sabin@mithrai.com>
2019-11-15 16:03:38 +0530

    React hook effect - Infant Sabin

--- ReactFeatures/ReactHooks.js
+++ ReactFeatures/ReactHooks.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 
 function ReactHooks () {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `you clicked ${count} times`
+  })
+
   return (
     <div>
       <NavLink to='/'>Back</NavLink>
